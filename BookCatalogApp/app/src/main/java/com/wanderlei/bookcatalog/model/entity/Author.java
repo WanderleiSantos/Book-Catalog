234c61058362f2189a16347251a6238524078529
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2016-02-12 22:01:06 -0200

    Ajustando tela de books, adicionando entidades novas

---
+++ BookCatalogApp/app/src/main/java/com/wanderlei/bookcatalog/model/entity/Author.java
@@ -0,0 +1,7 @@
+package com.wanderlei.bookcatalog.model.entity;
+
+/**
+ * Created by wanderlei on 12/02/16.
+ */
+public class Author {
+}
--- BookCatalogApp/app/src/main/java/com/wanderlei/bookcatalog/model/entity/Book.java
+++ BookCatalogApp/app/src/main/java/com/wanderlei/bookcatalog/model/entity/Book.java
@@ -8,101 +8,18 @@ import java.util.Date;
 public class Book {
 
     private String Kind;
-    private int id;
+    private String id;
     private String title;
     private String description;
-    private String access;
-    private Date updated;
-    private Date created;
-    private int volumeCount;
-    private Date volumesLastUpdated;
+    private String author;
+    private String publisher;
+    private Date publishedDate;
+    private String isbn_13;
+    private String isbn_10;
+    private int pageCount;
+    private String printType;
+    private String categories;
+    private String smallThumbnail;
+    private String thumbnail;
 
-    public Book(){
-
-    }
-
-    public Book(String kind, int id, String title, String description, String access, Date updated, Date created, int volumeCount, Date volumesLastUpdated) {
-        Kind = kind;
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.access = access;
-        this.updated = updated;
-        this.created = created;
-        this.volumeCount = volumeCount;
-        this.volumesLastUpdated = volumesLastUpdated;
-    }
-
-
-    public String getKind() {
-        return Kind;
-    }
-
-    public void setKind(String kind) {
-        Kind = kind;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAccess() {
-        return access;
-    }
-
-    public void setAccess(String access) {
-        this.access = access;
-    }
-
-    public Date getUpdated() {
-        return updated;
-    }
-
-    public void setUpdated(Date updated) {
-        this.updated = updated;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public int getVolumeCount() {
-        return volumeCount;
-    }
-
-    public void setVolumeCount(int volumeCount) {
-        this.volumeCount = volumeCount;
-    }
-
-    public Date getVolumesLastUpdated() {
-        return volumesLastUpdated;
-    }
-
-    public void setVolumesLastUpdated(Date volumesLastUpdated) {
-        this.volumesLastUpdated = volumesLastUpdated;
-    }
 }
